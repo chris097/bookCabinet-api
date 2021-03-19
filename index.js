2249cde2972ed24f20dfa5e6b656f061bcca4486
chris097 <chrisfidel.international@gmail.com>
2021-03-19 16:02:00 +0100

    add DB to the app

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const book = require('./routes/book')
 const comment = require('./routes/comment')
 const app = express()
 
-mongoose.connect('mongodb+srv://okeyhacker:okeyhacker@lib-shelter.z5ex1.mongodb.net/okeyhacker?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://chris:chris@book-db.z5ex1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoDB...'))
+// .catch(err => console.error('Could not connect to MongoDB...'))
 
 app.use(express.json())
 app.use('/api/books', book)
